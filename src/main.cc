e02dd2b8c525e48c4782a9ded7b38f5840bdd555
Konstantinos Sideris <sideris.konstantin@gmail.com>
2017-04-15 17:23:35 +0300

    Move the main window to the center

--- src/main.cc
+++ src/main.cc
@@ -16,6 +16,7 @@
  */
 
 #include <QApplication>
+#include <QDesktopWidget>
 #include <QFontDatabase>
 
 #include "MainWindow.h"
@@ -42,6 +43,13 @@ int main(int argc, char *argv[])
 	app.setFont(font);
 
 	MainWindow w;
+
+	// Move the MainWindow to the center
+	QRect screenGeometry = QApplication::desktop()->screenGeometry();
+	int x = (screenGeometry.width() - w.width()) / 2;
+	int y = (screenGeometry.height() - w.height()) / 2;
+
+	w.move(x, y);
 	w.show();
 
 	return app.exec();
