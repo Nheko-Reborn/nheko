cf16fdffe50b557024237f9b4d3dedab23b21876
Konstantinos Sideris <sideris.konstantin@gmail.com>
2017-05-29 20:31:56 +0300

    Fill out translation fields for english

--- resources/langs/nheko_en.ts
+++ resources/langs/nheko_en.ts
@@ -6,42 +6,42 @@
     <message>
         <location filename="../../src/EmojiPanel.cc" line="120"/>
         <source>Smileys &amp; People</source>
-        <translation></translation>
+        <translation>Smileys &amp; People</translation>
     </message>
     <message>
         <location filename="../../src/EmojiPanel.cc" line="123"/>
         <source>Animals &amp; Nature</source>
-        <translation></translation>
+        <translation>Animals &amp; Nature</translation>
     </message>
     <message>
         <location filename="../../src/EmojiPanel.cc" line="126"/>
         <source>Food &amp; Drink</source>
-        <translation></translation>
+        <translation>Food &amp; Drink</translation>
     </message>
     <message>
         <location filename="../../src/EmojiPanel.cc" line="129"/>
         <source>Activity</source>
-        <translation></translation>
+        <translation>Activity</translation>
     </message>
     <message>
         <location filename="../../src/EmojiPanel.cc" line="132"/>
         <source>Travel &amp; Places</source>
-        <translation></translation>
+        <translation>Travel &amp; Places</translation>
     </message>
     <message>
         <location filename="../../src/EmojiPanel.cc" line="135"/>
         <source>Objects</source>
-        <translation></translation>
+        <translation>Objects</translation>
     </message>
     <message>
         <location filename="../../src/EmojiPanel.cc" line="138"/>
         <source>Symbols</source>
-        <translation></translation>
+        <translation>Symbols</translation>
     </message>
     <message>
         <location filename="../../src/EmojiPanel.cc" line="141"/>
         <source>Flags</source>
-        <translation></translation>
+        <translation>Flags</translation>
     </message>
 </context>
 <context>
@@ -49,32 +49,32 @@
     <message>
         <location filename="../../src/LoginPage.cc" line="85"/>
         <source>Matrix ID</source>
-        <translation></translation>
+        <translation>Matrix ID</translation>
     </message>
     <message>
         <location filename="../../src/LoginPage.cc" line="88"/>
         <source>e.g @joe:matrix.org</source>
-        <translation></translation>
+        <translation>e.g @joe:matrix.org</translation>
     </message>
     <message>
         <location filename="../../src/LoginPage.cc" line="92"/>
         <source>Password</source>
-        <translation></translation>
+        <translation>Password</translation>
     </message>
     <message>
         <location filename="../../src/LoginPage.cc" line="104"/>
         <source>LOGIN</source>
-        <translation></translation>
+        <translation>LOGIN</translation>
     </message>
     <message>
         <location filename="../../src/LoginPage.cc" line="150"/>
         <source>Invalid Matrix ID</source>
-        <translation></translation>
+        <translation>Invalid Matrix ID</translation>
     </message>
     <message>
         <location filename="../../src/LoginPage.cc" line="152"/>
         <source>Empty password</source>
-        <translation></translation>
+        <translation>Empty password</translation>
     </message>
 </context>
 <context>
@@ -82,22 +82,22 @@
     <message>
         <location filename="../../src/MatrixClient.cc" line="75"/>
         <source>Wrong username or password</source>
-        <translation></translation>
+        <translation>Wrong username or password</translation>
     </message>
     <message>
         <location filename="../../src/MatrixClient.cc" line="80"/>
         <source>Login endpoint was not found on the server</source>
-        <translation></translation>
+        <translation>Login endpoint was not found on the server</translation>
     </message>
     <message>
         <location filename="../../src/MatrixClient.cc" line="86"/>
         <source>An unknown error occured. Please try again.</source>
-        <translation></translation>
+        <translation>An unknown error occured. Please try again.</translation>
     </message>
     <message>
         <location filename="../../src/MatrixClient.cc" line="100"/>
         <source>Malformed response. Possibly not a Matrix server</source>
-        <translation></translation>
+        <translation>Malformed response. Possibly not a Matrix server</translation>
     </message>
 </context>
 <context>
@@ -105,47 +105,47 @@
     <message>
         <location filename="../../src/RegisterPage.cc" line="72"/>
         <source>Username</source>
-        <translation></translation>
+        <translation>Username</translation>
     </message>
     <message>
         <location filename="../../src/RegisterPage.cc" line="78"/>
         <source>Password</source>
-        <translation></translation>
+        <translation>Password</translation>
     </message>
     <message>
         <location filename="../../src/RegisterPage.cc" line="85"/>
         <source>Password confirmation</source>
-        <translation></translation>
+        <translation>Password confirmation</translation>
     </message>
     <message>
         <location filename="../../src/RegisterPage.cc" line="92"/>
         <source>Home Server</source>
-        <translation></translation>
+        <translation>Home Server</translation>
     </message>
     <message>
         <location filename="../../src/RegisterPage.cc" line="108"/>
         <source>REGISTER</source>
-        <translation></translation>
+        <translation>REGISTER</translation>
     </message>
     <message>
         <location filename="../../src/RegisterPage.cc" line="159"/>
         <source>Invalid username</source>
-        <translation></translation>
+        <translation>Invalid username</translation>
     </message>
     <message>
         <location filename="../../src/RegisterPage.cc" line="161"/>
         <source>Password is not long enough (min 8 chars)</source>
-        <translation></translation>
+        <translation>Password is not long enough (min 8 chars)</translation>
     </message>
     <message>
         <location filename="../../src/RegisterPage.cc" line="163"/>
         <source>Passwords don&apos;t match</source>
-        <translation></translation>
+        <translation>Passwords don&apos;t match</translation>
     </message>
     <message>
         <location filename="../../src/RegisterPage.cc" line="165"/>
         <source>Invalid server name</source>
-        <translation></translation>
+        <translation>Invalid server name</translation>
     </message>
 </context>
 <context>
@@ -153,7 +153,7 @@
     <message>
         <location filename="../../src/TextInputWidget.cc" line="62"/>
         <source>Write a message...</source>
-        <translation></translation>
+        <translation>Write a message...</translation>
     </message>
 </context>
 <context>
@@ -161,12 +161,12 @@
     <message>
         <location filename="../../src/TrayIcon.cc" line="73"/>
         <source>Show</source>
-        <translation></translation>
+        <translation>Show</translation>
     </message>
     <message>
         <location filename="../../src/TrayIcon.cc" line="74"/>
         <source>Quit</source>
-        <translation></translation>
+        <translation>Quit</translation>
     </message>
 </context>
 <context>
@@ -174,22 +174,22 @@
     <message>
         <location filename="../../src/WelcomePage.cc" line="38"/>
         <source>Welcome to nheko! The desktop client for the Matrix protocol.</source>
-        <translation></translation>
+        <translation>Welcome to nheko! The desktop client for the Matrix protocol.</translation>
     </message>
     <message>
         <location filename="../../src/WelcomePage.cc" line="39"/>
         <source>Enjoy your stay!</source>
-        <translation></translation>
+        <translation>Enjoy your stay!</translation>
     </message>
     <message>
         <location filename="../../src/WelcomePage.cc" line="60"/>
         <source>REGISTER</source>
-        <translation></translation>
+        <translation>REGISTER</translation>
     </message>
     <message>
         <location filename="../../src/WelcomePage.cc" line="68"/>
         <source>LOGIN</source>
-        <translation></translation>
+        <translation>LOGIN</translation>
     </message>
 </context>
 </TS>
