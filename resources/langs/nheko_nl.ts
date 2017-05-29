484f62e5389db977cab61529a0ff6a9282f92013
Bart Ribbers <bart.ribbers@openmailbox.org>
2017-05-29 17:49:11 +0000

    Translated using Weblate (Dutch)
    Currently translated at 97.0% (33 of 34 strings)
    
    Translation: Nheko/Nheko
    Translate-URL: https://translate.nordgedanken.de/projects/nheko/nheko/nl/

--- resources/langs/nheko_nl.ts
+++ resources/langs/nheko_nl.ts
@@ -6,42 +6,42 @@
     <message>
         <location filename="../../src/EmojiPanel.cc" line="120"/>
         <source>Smileys &amp; People</source>
-        <translation type="unfinished"></translation>
+        <translation>Smileys &amp; Mensen</translation>
     </message>
     <message>
         <location filename="../../src/EmojiPanel.cc" line="123"/>
         <source>Animals &amp; Nature</source>
-        <translation type="unfinished"></translation>
+        <translation>Dieren &amp; Natuur</translation>
     </message>
     <message>
         <location filename="../../src/EmojiPanel.cc" line="126"/>
         <source>Food &amp; Drink</source>
-        <translation type="unfinished"></translation>
+        <translation>Eten &amp; Drinken</translation>
     </message>
     <message>
         <location filename="../../src/EmojiPanel.cc" line="129"/>
         <source>Activity</source>
-        <translation type="unfinished"></translation>
+        <translation>Activiteit</translation>
     </message>
     <message>
         <location filename="../../src/EmojiPanel.cc" line="132"/>
         <source>Travel &amp; Places</source>
-        <translation type="unfinished"></translation>
+        <translation>Reizen &amp; Locaties</translation>
     </message>
     <message>
         <location filename="../../src/EmojiPanel.cc" line="135"/>
         <source>Objects</source>
-        <translation type="unfinished"></translation>
+        <translation>Objecten</translation>
     </message>
     <message>
         <location filename="../../src/EmojiPanel.cc" line="138"/>
         <source>Symbols</source>
-        <translation type="unfinished"></translation>
+        <translation>Symbolen</translation>
     </message>
     <message>
         <location filename="../../src/EmojiPanel.cc" line="141"/>
         <source>Flags</source>
-        <translation type="unfinished"></translation>
+        <translation>Vlaggen</translation>
     </message>
 </context>
 <context>
@@ -49,32 +49,32 @@
     <message>
         <location filename="../../src/LoginPage.cc" line="85"/>
         <source>Matrix ID</source>
-        <translation type="unfinished"></translation>
+        <translation>Matrix ID</translation>
     </message>
     <message>
         <location filename="../../src/LoginPage.cc" line="88"/>
         <source>e.g @joe:matrix.org</source>
-        <translation type="unfinished"></translation>
+        <translation>b.v @jan:matrix.org</translation>
     </message>
     <message>
         <location filename="../../src/LoginPage.cc" line="92"/>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Wachtwoord</translation>
     </message>
     <message>
         <location filename="../../src/LoginPage.cc" line="104"/>
         <source>LOGIN</source>
-        <translation type="unfinished"></translation>
+        <translation>LOGIN</translation>
     </message>
     <message>
         <location filename="../../src/LoginPage.cc" line="150"/>
         <source>Invalid Matrix ID</source>
-        <translation type="unfinished"></translation>
+        <translation>Ongeldig Matrix ID</translation>
     </message>
     <message>
         <location filename="../../src/LoginPage.cc" line="152"/>
         <source>Empty password</source>
-        <translation type="unfinished"></translation>
+        <translation>Leeg wachtwoord</translation>
     </message>
 </context>
 <context>
@@ -82,22 +82,22 @@
     <message>
         <location filename="../../src/MatrixClient.cc" line="75"/>
         <source>Wrong username or password</source>
-        <translation type="unfinished"></translation>
+        <translation>Verkeerde gebruikersnaam of wachtwoord</translation>
     </message>
     <message>
         <location filename="../../src/MatrixClient.cc" line="80"/>
         <source>Login endpoint was not found on the server</source>
-        <translation type="unfinished"></translation>
+        <translation>De login eindpunt is niet gevonden op de server</translation>
     </message>
     <message>
         <location filename="../../src/MatrixClient.cc" line="86"/>
         <source>An unknown error occured. Please try again.</source>
-        <translation type="unfinished"></translation>
+        <translation>Er is een onbekende fout opgetreden. Probeer het alsjeblieft opnieuw.</translation>
     </message>
     <message>
         <location filename="../../src/MatrixClient.cc" line="100"/>
         <source>Malformed response. Possibly not a Matrix server</source>
-        <translation type="unfinished"></translation>
+        <translation>Onjuist antwoord ontvangen. Het is mogelijk geen Matrix server</translation>
     </message>
 </context>
 <context>
@@ -105,47 +105,47 @@
     <message>
         <location filename="../../src/RegisterPage.cc" line="72"/>
         <source>Username</source>
-        <translation type="unfinished"></translation>
+        <translation>Gebruikersnaam</translation>
     </message>
     <message>
         <location filename="../../src/RegisterPage.cc" line="78"/>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Wachtwoord</translation>
     </message>
     <message>
         <location filename="../../src/RegisterPage.cc" line="85"/>
         <source>Password confirmation</source>
-        <translation type="unfinished"></translation>
+        <translation>Wachtwoord bevestiging</translation>
     </message>
     <message>
         <location filename="../../src/RegisterPage.cc" line="92"/>
         <source>Home Server</source>
-        <translation type="unfinished"></translation>
+        <translation>Home server</translation>
     </message>
     <message>
         <location filename="../../src/RegisterPage.cc" line="108"/>
         <source>REGISTER</source>
-        <translation type="unfinished"></translation>
+        <translation>REGISTREER</translation>
     </message>
     <message>
         <location filename="../../src/RegisterPage.cc" line="159"/>
         <source>Invalid username</source>
-        <translation type="unfinished"></translation>
+        <translation>Ongeldige gebruikersnaam</translation>
     </message>
     <message>
         <location filename="../../src/RegisterPage.cc" line="161"/>
         <source>Password is not long enough (min 8 chars)</source>
-        <translation type="unfinished"></translation>
+        <translation>Het wachtwoord is niet lang genoeg (minimaal 8 karakters)</translation>
     </message>
     <message>
         <location filename="../../src/RegisterPage.cc" line="163"/>
-        <source>Passwords don&apos;t match</source>
-        <translation type="unfinished"></translation>
+        <source>Passwords don't match</source>
+        <translation>De wachtwoorden komen niet overeen</translation>
     </message>
     <message>
         <location filename="../../src/RegisterPage.cc" line="165"/>
         <source>Invalid server name</source>
-        <translation type="unfinished"></translation>
+        <translation>Ongeldige server naam</translation>
     </message>
 </context>
 <context>
@@ -153,7 +153,7 @@
     <message>
         <location filename="../../src/TextInputWidget.cc" line="62"/>
         <source>Write a message...</source>
-        <translation type="unfinished"></translation>
+        <translation>Type een bericht...</translation>
     </message>
 </context>
 <context>
@@ -161,12 +161,12 @@
     <message>
         <location filename="../../src/TrayIcon.cc" line="73"/>
         <source>Show</source>
-        <translation type="unfinished"></translation>
+        <translation>Laten zien</translation>
     </message>
     <message>
         <location filename="../../src/TrayIcon.cc" line="74"/>
         <source>Quit</source>
-        <translation type="unfinished"></translation>
+        <translation>Sluiten</translation>
     </message>
 </context>
 <context>
@@ -174,22 +174,22 @@
     <message>
         <location filename="../../src/WelcomePage.cc" line="38"/>
         <source>Welcome to nheko! The desktop client for the Matrix protocol.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Welkom bij nheko! De desktop client voor het Matrix protocol.</translation>
     </message>
     <message>
         <location filename="../../src/WelcomePage.cc" line="39"/>
         <source>Enjoy your stay!</source>
-        <translation type="unfinished"></translation>
+        <translation>Geniet van je verblijf!</translation>
     </message>
     <message>
         <location filename="../../src/WelcomePage.cc" line="60"/>
         <source>REGISTER</source>
-        <translation type="unfinished"></translation>
+        <translation>REGISTREER</translation>
     </message>
     <message>
         <location filename="../../src/WelcomePage.cc" line="68"/>
         <source>LOGIN</source>
-        <translation type="unfinished"></translation>
+        <translation>LOGIN</translation>
     </message>
 </context>
 </TS>
