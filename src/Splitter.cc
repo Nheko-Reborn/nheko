82b48d926dd835aa3adb5fb3a8426532386df5c0
Konstantinos Sideris <sideris.konstantin@gmail.com>
2017-06-06 15:03:11 +0300

    Don't collapse room list

--- src/Splitter.cc
+++ src/Splitter.cc
@@ -24,6 +24,7 @@ Splitter::Splitter(QWidget *parent)
     : QSplitter(parent)
 {
 	connect(this, &QSplitter::splitterMoved, this, &Splitter::onSplitterMoved);
+	setChildrenCollapsible(false);
 }
 
 void Splitter::onSplitterMoved(int pos, int index)
