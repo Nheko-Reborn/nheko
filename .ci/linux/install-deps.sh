4a912a2dff31468468b6fd544653e25c82568157
Konstantinos Sideris <sideris.konstantin@gmail.com>
2017-11-10 22:06:26 +0200

    Update travis to qt5.9.2

--- .ci/linux/install-deps.sh
+++ .ci/linux/install-deps.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo add-apt-repository -y ppa:beineri/opt-qt58-trusty
+sudo add-apt-repository -y ppa:beineri/opt-qt592-trusty
 sudo add-apt-repository -y ppa:george-edison55/cmake-3.x
 sudo apt-get update -qq
-sudo apt-get install -qq -y qt58base qt58tools cmake liblmdb-dev
+sudo apt-get install -qq -y qt59base qt59tools cmake liblmdb-dev
