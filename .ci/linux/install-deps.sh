78353a29bce717b46a5aa720927abb88faa53f44
Konstantinos Sideris <sideris.konstantin@gmail.com>
2017-12-01 15:53:30 +0200

    Install qt-multimedia on travis

--- .ci/linux/install-deps.sh
+++ .ci/linux/install-deps.sh
@@ -3,4 +3,4 @@
 sudo add-apt-repository -y ppa:beineri/opt-qt592-trusty
 sudo add-apt-repository -y ppa:george-edison55/cmake-3.x
 sudo apt-get update -qq
-sudo apt-get install -qq -y qt59base qt59tools cmake liblmdb-dev
+sudo apt-get install -qq -y qt59base qt59tools cmake liblmdb-dev qt59multimedia
