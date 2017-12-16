bd512533fbf9c1f7598973fe777e474deee0341b
Jani Mustonen <janijohannes@kapsi.fi>
2017-12-16 18:05:14 +0200

    Update package.nix to include QT Multimedia (#165)

--- package.nix
+++ package.nix
@@ -1,4 +1,4 @@
-{ pkgs, stdenv, qtbase, qttranslations, lmdb, mkDerivation, cmake }:
+{ pkgs, stdenv, qtbase, qttranslations, qtmultimedia, lmdb, mkDerivation, cmake }:
 stdenv.mkDerivation rec {
   version = "0.1.0";
   name = "nheko-${version}";
@@ -9,7 +9,7 @@ stdenv.mkDerivation rec {
            (type == "symlink" && name == "result") ))
     ./.;
   nativeBuildInputs = [ cmake ];
-  buildInputs = [ qtbase qttranslations lmdb ];
+  buildInputs = [ qtbase qttranslations qtmultimedia lmdb ];
   installPhase = ''
     mkdir -p $out/bin
     cp nheko $out/bin/nheko
