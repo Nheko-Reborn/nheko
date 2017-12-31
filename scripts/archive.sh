799885ad243076b2436ec7943b585c4da3e7ea08
Konstantinos Sideris <sideris.konstantin@gmail.com>
2017-12-31 14:27:25 +0200

    Add script to create archive
    [ci skip]

--- scripts/archive.sh
+++ scripts/archive.sh
@@ -1,12 +1,6 @@
 #!/bin/bash -e
 
-TAG=`git tag -l --points-at HEAD`
 PREFIX=$(basename "$(pwd -P)")
-
-if [ ! -z $TAG ]; then
-    PREFIX=$(basename "$(pwd -P)").$TAG
-fi
-
 {
   git ls-files
   git submodule foreach --recursive --quiet \
