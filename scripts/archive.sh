b70d11fb6fd1135932a173c2a7a854a8874d9af5
Konstantinos Sideris <sideris.konstantin@gmail.com>
2018-01-02 11:00:16 +0200

    Add missing -j flag to the archive script
    [ci skip]

--- scripts/archive.sh
+++ scripts/archive.sh
@@ -5,4 +5,4 @@ PREFIX=$(basename "$(pwd -P)")
   git ls-files
   git submodule foreach --recursive --quiet \
                 'git ls-files --with-tree="$sha1" | sed "s#^#$path/#"'
-} | sed "s#^#$PREFIX/#" | xargs tar -c -C.. -f "$PREFIX.tar.bz2" --
+} | sed "s#^#$PREFIX/#" | xargs tar -c -j -C.. -f "$PREFIX.tar.bz2" --
