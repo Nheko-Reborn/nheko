Nheko-Reborn/nheko

commit 9de1ec1b7b521227e9df2850be19242b4876ed95

Fix crash when inviting users

src/LoginPage.cc
@@ -17,15 +17,18 @@
 
 #include <QStyleOption>
 
+#include <mtx/identifiers.hpp>
+
 #include "Config.h"
 #include "FlatButton.h"
-#include "InputValidator.h"
 #include "LoadingIndicator.h"
 #include "LoginPage.h"
 #include "MatrixClient.h"
 #include "OverlayModal.h"
 #include "RaisedButton.h"
 #include "TextField.h"
+
+using namespace mtx::identifiers;
 
 LoginPage::LoginPage(QSharedPointer<MatrixClient> client, QWidget *parent)
   : QWidget(parent)
@@ -157,10 +160,16 @@ LoginPage::loginError(QString error)
 bool
 LoginPage::isMatrixIdValid()
 {
-        int pos        = 0;
         auto matrix_id = matrixid_input_->text();
 
-        return InputValidator::Id.validate(matrix_id, pos) == QValidator::Acceptable;
+        try {
+                parse<User>(matrix_id.toStdString());
+                return true;
+        } catch (const std::exception &e) {
+                return false;
+        }
+
+        return false;
 }
 
 void

CMakeLists.txt
@@ -195,7 +195,6 @@ set(SRC_FILES
     src/Community.cc
     src/Deserializable.cc
     src/InviteeItem.cc
-    src/InputValidator.cc
     src/LoginPage.cc
     src/MainWindow.cc
     src/MatrixClient.cc

include/InputValidator.h
@@ -1,30 +1,0 @@
-/*
- * nheko Copyright (C) 2017  Konstantinos Sideris <siderisk@auth.gr>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-#pragma once
-
-#include <QRegExpValidator>
-
-class InputValidator
-{
-public:
-        // Validators for the different types of input.
-        static QRegExpValidator Id;
-        static QRegExpValidator Localpart;
-        static QRegExpValidator Password;
-        static QRegExpValidator Domain;
-};

src/ChatPage.cc
@@ -149,7 +149,7 @@ ChatPage::ChatPage(QSharedPointer<MatrixClient> client,
 
         connect(top_bar_, &TopRoomBar::inviteUsers, this, [this](QStringList users) {
                 for (int ii = 0; ii < users.size(); ++ii) {
-                        QTimer::singleShot(ii * 1000, this, [this, &ii, &users]() {
+                        QTimer::singleShot(ii * 1000, this, [this, ii, users]() {
                                 client_->inviteUser(current_room_, users.at(ii));
                         });
                 }

src/InputValidator.cc
@@ -1,29 +1,0 @@
-/*
- * nheko Copyright (C) 2017  Konstantinos Sideris <siderisk@auth.gr>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-#include "InputValidator.h"
-
-const QRegExp MXID_REGEX("@[A-Za-z0-9._%+-]+:[A-Za-z0-9.-]{1,126}\\.[A-Za-z]{1,63}");
-const QRegExp LOCALPART_REGEX("[A-za-z0-9._%+-]{3,}");
-const QRegExp PASSWORD_REGEX(".{8,}");
-const QRegExp DOMAIN_REGEX("(?!\\-)(?:[a-zA-Z\\d\\-]{0,62}[a-zA-Z\\d]\\.){1,"
-                           "126}(?!\\d+)[a-zA-Z\\d]{1,63}");
-
-QRegExpValidator InputValidator::Id(MXID_REGEX);
-QRegExpValidator InputValidator::Localpart(LOCALPART_REGEX);
-QRegExpValidator InputValidator::Password(PASSWORD_REGEX);
-QRegExpValidator InputValidator::Domain(DOMAIN_REGEX);

src/RegisterPage.cc
@@ -19,7 +19,6 @@
 
 #include "Config.h"
 #include "FlatButton.h"
-#include "InputValidator.h"
 #include "MatrixClient.h"
 #include "RaisedButton.h"
 #include "RegisterPage.h"
@@ -127,10 +126,6 @@ RegisterPage::RegisterPage(QSharedPointer<MatrixClient> client, QWidget *parent)
                 SIGNAL(registerError(const QString &)),
                 this,
                 SLOT(registerError(const QString &)));
-
-        username_input_->setValidator(&InputValidator::Localpart);
-        password_input_->setValidator(&InputValidator::Password);
-        server_input_->setValidator(&InputValidator::Domain);
 
         setLayout(top_layout_);
 }

commit 735d508a29f2e67f1082ee922ee61ce9dec21c8a

Handle empty matrix ID (regression from 9de1ec1)
fixes #259

include/LoginPage.h
@@ -48,7 +48,7 @@ protected:
 
 public slots:
         // Displays errors produced during the login.
-        void loginError(QString error_message);
+        void loginError(QString msg) { error_label_->setText(msg); }
 
 private slots:
         // Callback for the back button.

src/LoginPage.cc
@@ -152,39 +152,22 @@ LoginPage::LoginPage(QSharedPointer<MatrixClient> client, QWidget *parent)
 }
 
 void
-LoginPage::loginError(QString error)
-{
-        error_label_->setText(error);
-}
-
-bool
-LoginPage::isMatrixIdValid()
-{
-        auto matrix_id = matrixid_input_->text();
-
-        try {
-                parse<User>(matrix_id.toStdString());
-                return true;
-        } catch (const std::exception &e) {
-                return false;
-        }
-
-        return false;
-}
-
-void
 LoginPage::onMatrixIdEntered()
 {
         error_label_->setText("");
 
-        if (!isMatrixIdValid()) {
-                loginError("You have entered an invalid Matrix ID  e.g @joe:matrix.org");
-                return;
-        } else if (password_input_->text().isEmpty()) {
-                loginError(tr("Empty password"));
+        User user;
+
+        try {
+                user = parse<User>(matrixid_input_->text().toStdString());
+        } catch (const std::exception &e) {
+                return loginError("You have entered an invalid Matrix ID  e.g @joe:matrix.org");
         }
 
-        QString homeServer = matrixid_input_->text().split(":").at(1);
+        if (password_input_->text().isEmpty())
+                return loginError(tr("Empty password"));
+
+        QString homeServer = QString::fromStdString(user.hostname());
         if (homeServer != inferredServerAddress_) {
                 serverInput_->hide();
                 serverLayout_->removeWidget(errorIcon_);
@@ -251,17 +234,21 @@ LoginPage::onLoginButtonClicked()
 {
         error_label_->setText("");
 
-        if (!isMatrixIdValid()) {
-                loginError("You have entered an invalid Matrix ID  e.g @joe:matrix.org");
-        } else if (password_input_->text().isEmpty()) {
-                loginError("Empty password");
-        } else {
-                QString user     = matrixid_input_->text().split(":").at(0).split("@").at(1);
-                QString password = password_input_->text();
-                client_->setServer(serverInput_->text());
-                client_->login(user, password);
-                emit loggingIn();
+        User user;
+
+        try {
+                user = parse<User>(matrixid_input_->text().toStdString());
+        } catch (const std::exception &e) {
+                return loginError("You have entered an invalid Matrix ID  e.g @joe:matrix.org");
         }
+
+        if (password_input_->text().isEmpty())
+                return loginError(tr("Empty password"));
+
+        client_->setServer(serverInput_->text());
+        client_->login(QString::fromStdString(user.localpart()), password_input_->text());
+
+        emit loggingIn();
 }
 
 void

src/MatrixClient.cc
@@ -123,11 +123,6 @@ MatrixClient::login(const QString &username, const QString &password) noexcept
                 int status_code =
                   reply->attribute(QNetworkRequest::HttpStatusCodeAttribute).toInt();
 
-                if (reply->error()) {
-                        emit loginError(reply->errorString());
-                        return;
-                }
-
                 if (status_code == 403) {
                         emit loginError(tr("Wrong username or password"));
                         return;
@@ -141,6 +136,11 @@ MatrixClient::login(const QString &username, const QString &password) noexcept
                 if (status_code >= 400) {
                         qWarning() << "Login error: " << reply->errorString();
                         emit loginError(tr("An unknown error occured. Please try again."));
+                        return;
+                }
+
+                if (reply->error()) {
+                        emit loginError(reply->errorString());
                         return;
                 }
 