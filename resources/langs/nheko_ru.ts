fab413327c63d9ce3bf10c0bef827d4b105662a2
Anton Karmanov <bergentroll@insiberia.net>
2018-05-15 00:09:22 +0500

    Initial Russian translate (#318)

---
+++ resources/langs/nheko_ru.ts
@@ -0,0 +1,559 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru">
+<context>
+    <name>AudioItem</name>
+    <message>
+        <location filename="../../src/timeline/widgets/AudioItem.cc" line="+125"/>
+        <source>Save File</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>DateSeparator</name>
+    <message>
+        <location filename="../../src/timeline/TimelineView.cc" line="+54"/>
+        <source>Today</source>
+        <translation>Сегодня</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Yesterday</source>
+        <translation>Вчера</translation>
+    </message>
+</context>
+<context>
+    <name>FileItem</name>
+    <message>
+        <location filename="../../src/timeline/widgets/FileItem.cc" line="+111"/>
+        <source>Save File</source>
+        <translation>Сохранить файл</translation>
+    </message>
+</context>
+<context>
+    <name>ImageItem</name>
+    <message>
+        <location filename="../../src/timeline/widgets/ImageItem.cc" line="+229"/>
+        <source>Save image</source>
+        <translation>Сохранить изображение</translation>
+    </message>
+</context>
+<context>
+    <name>LoginPage</name>
+    <message>
+        <location filename="../../src/LoginPage.cc" line="+79"/>
+        <source>Matrix ID</source>
+        <translation>Идентификатор Matrix</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>e.g @joe:matrix.org</source>
+        <translation>Пример: @joe:matrix.org</translation>
+    </message>
+    <message>
+        <location line="+15"/>
+        <source>Password</source>
+        <translation>Пароль</translation>
+    </message>
+    <message>
+        <location line="+19"/>
+        <source>LOGIN</source>
+        <translation>ВОЙТИ</translation>
+    </message>
+    <message>
+        <location line="+128"/>
+        <source>Empty password</source>
+        <translation>Пустой пароль</translation>
+    </message>
+</context>
+<context>
+    <name>MatrixClient</name>
+    <message>
+        <location filename="../../src/MatrixClient.cc" line="+164"/>
+        <source>Wrong username or password</source>
+        <translation>Неверное имя пользователя или пароль</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>Login endpoint was not found on the server</source>
+        <translation>Такой логин не найден на указанном сервере</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>An unknown error occured. Please try again.</source>
+        <translation>Произошла неизвестная ошибка. Попробуйте снова.</translation>
+    </message>
+    <message>
+        <location line="+23"/>
+        <source>Malformed response. Possibly not a Matrix server</source>
+        <translation>Ответ не распознан. Возможно это не сервер Matrix?</translation>
+    </message>
+</context>
+<context>
+    <name>MemberList</name>
+    <message>
+        <location filename="../../src/dialogs/MemberList.cpp" line="+79"/>
+        <source>Room members</source>
+        <translation>Участники комнаты</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>SHOW MORE</source>
+        <translation>ПОКАЗАТЬ ЕЩЁ</translation>
+    </message>
+</context>
+<context>
+    <name>QuickSwitcher</name>
+    <message>
+        <location filename="../../src/QuickSwitcher.cc" line="+70"/>
+        <source>Search for a room...</source>
+        <translation>Поиск комнаты...</translation>
+    </message>
+</context>
+<context>
+    <name>RegisterPage</name>
+    <message>
+        <location filename="../../src/RegisterPage.cc" line="+76"/>
+        <source>Username</source>
+        <translation>Имя пользователя</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Password</source>
+        <translation>Пароль</translation>
+    </message>
+    <message>
+        <location line="+4"/>
+        <source>Password confirmation</source>
+        <translation>Подтверждение пароля</translation>
+    </message>
+    <message>
+        <location line="+4"/>
+        <source>Home Server</source>
+        <translation>Домашний сервер</translation>
+    </message>
+    <message>
+        <location line="+17"/>
+        <source>REGISTER</source>
+        <translation>ЗАРЕГЕСТРИРОВАН</translation>
+    </message>
+    <message>
+        <location line="+76"/>
+        <source>Invalid username</source>
+        <translation>Некорректное имя пользователя</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Password is not long enough (min 8 chars)</source>
+        <translation>Слишком короткий пароль (минимум 8 символов)</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Passwords don&apos;t match</source>
+        <translation>Пароли не совпадают</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Invalid server name</source>
+        <translation>Неверное имя сервера</translation>
+    </message>
+</context>
+<context>
+    <name>RoomInfoListItem</name>
+    <message>
+        <location filename="../../src/RoomInfoListItem.cc" line="+78"/>
+        <source>Leave room</source>
+        <translation>Покинуть комнату</translation>
+    </message>
+    <message>
+        <location line="+153"/>
+        <source>Accept</source>
+        <translation>Принять</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Decline</source>
+        <translation>Отказаться</translation>
+    </message>
+</context>
+<context>
+    <name>SideBarActions</name>
+    <message>
+        <location filename="../../src/SideBarActions.cc" line="+36"/>
+        <source>Create new room</source>
+        <translation>Создать новую комнату</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Join a room</source>
+        <translation>Присоединиться к комнате</translation>
+    </message>
+</context>
+<context>
+    <name>TextInputWidget</name>
+    <message>
+        <location filename="../../src/TextInputWidget.cc" line="+445"/>
+        <source>Write a message...</source>
+        <translation>Написать сообщение...</translation>
+    </message>
+    <message>
+        <location line="+108"/>
+        <source>Select a file</source>
+        <translation>Выберите файл</translation>
+    </message>
+    <message>
+        <location line="+0"/>
+        <source>All Files (*)</source>
+        <translation>Все файлы (*)</translation>
+    </message>
+</context>
+<context>
+    <name>TopRoomBar</name>
+    <message>
+        <location filename="../../src/TopRoomBar.cc" line="+87"/>
+        <source>Invite users</source>
+        <translation>Пригласить пользователей</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Members</source>
+        <translation>Участники</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>Leave room</source>
+        <translation>Покинуть комнату</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+</context>
+<context>
+    <name>TrayIcon</name>
+    <message>
+        <location filename="../../src/TrayIcon.cc" line="+116"/>
+        <source>Show</source>
+        <translation>Показать</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Quit</source>
+        <translation>Выйти</translation>
+    </message>
+</context>
+<context>
+    <name>TypingDisplay</name>
+    <message>
+        <location filename="../../src/TypingDisplay.cc" line="+26"/>
+        <source> is typing</source>
+        <translation> вводится</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source> are typing</source>
+        <translation> вводятся</translation>
+    </message>
+</context>
+<context>
+    <name>UserSettingsPage</name>
+    <message>
+        <location filename="../../src/UserSettingsPage.cc" line="+121"/>
+        <source>User Settings</source>
+        <translation>Пользовательские настройки</translation>
+    </message>
+    <message>
+        <location line="+15"/>
+        <source>Minimize to tray</source>
+        <translation>Сворачивать в лоток</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>Start in tray</source>
+        <translation>Запускать в лотке</translation>
+    </message>
+    <message>
+        <location line="+12"/>
+        <source>Re-order rooms based on activity</source>
+        <translation>Упорядочивать комнаты по активности</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>Group&apos;s sidebar</source>
+        <translation>Боковая панель групп</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>Typing notifications</source>
+        <translation>Сообщать о наборе сообщения</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>Read receipts</source>
+        <translation>Подтверждать прочтение</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>Theme</source>
+        <translation>Тема</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>GENERAL</source>
+        <translation>ГЛАВНОЕ</translation>
+    </message>
+</context>
+<context>
+    <name>WelcomePage</name>
+    <message>
+        <location filename="../../src/WelcomePage.cc" line="+44"/>
+        <source>Welcome to nheko! The desktop client for the Matrix protocol.</source>
+        <translation>Добро пожаловать в nheko, клиент протокола Matrix!</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Enjoy your stay!</source>
+        <translation>Хорошо провести время!</translation>
+    </message>
+    <message>
+        <location line="+19"/>
+        <source>REGISTER</source>
+        <translation>РЕГИСТРАЦИЯ</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>LOGIN</source>
+        <translation>ВХОД</translation>
+    </message>
+</context>
+<context>
+    <name>dialogs::CreateRoom</name>
+    <message>
+        <location filename="../../src/dialogs/CreateRoom.cc" line="+32"/>
+        <source>CANCEL</source>
+        <translation>ОТМЕНА</translation>
+    </message>
+    <message>
+        <location line="+11"/>
+        <source>Name</source>
+        <translation>Название</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Topic</source>
+        <translation>Тема</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Alias</source>
+        <translation>Псевдоним</translation>
+    </message>
+    <message>
+        <location line="+8"/>
+        <source>Room Visibility</source>
+        <translation>Видимость комнаты</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>Room Preset</source>
+        <translation>Шаблон комнаты</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>Direct Chat</source>
+        <translation>Прямой чат</translation>
+    </message>
+</context>
+<context>
+    <name>dialogs::InviteUsers</name>
+    <message>
+        <location filename="../../src/dialogs/InviteUsers.cc" line="+36"/>
+        <source>CANCEL</source>
+        <translation>ОТМЕНА</translation>
+    </message>
+    <message>
+        <location line="+11"/>
+        <source>User ID to invite</source>
+        <translation>Идентификатор пользователя</translation>
+    </message>
+</context>
+<context>
+    <name>dialogs::JoinRoom</name>
+    <message>
+        <location filename="../../src/dialogs/JoinRoom.cc" line="+30"/>
+        <source>CANCEL</source>
+        <translation>ОТМЕНА</translation>
+    </message>
+    <message>
+        <location line="+11"/>
+        <source>Room ID or alias</source>
+        <translation>Идентификатор или псевдоним комнаты</translation>
+    </message>
+</context>
+<context>
+    <name>dialogs::LeaveRoom</name>
+    <message>
+        <location filename="../../src/dialogs/LeaveRoom.cc" line="+29"/>
+        <source>CANCEL</source>
+        <translation>ОТМЕНА</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>Are you sure you want to leave?</source>
+        <translation>Вы действительно желаете выйти?</translation>
+    </message>
+</context>
+<context>
+    <name>dialogs::Logout</name>
+    <message>
+        <location filename="../../src/dialogs/Logout.cc" line="+47"/>
+        <source>CANCEL</source>
+        <translation>ОТМЕНА</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>Logout. Are you sure?</source>
+        <translation>Выйти из учётной записи. Вы увереныee?</translation>
+    </message>
+</context>
+<context>
+    <name>dialogs::PreviewUploadOverlay</name>
+    <message>
+        <location filename="../../src/dialogs/PreviewUploadOverlay.cc" line="+41"/>
+        <source>Upload</source>
+        <translation>Загрузить</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Cancel</source>
+        <translation>Отменить</translation>
+    </message>
+    <message>
+        <location line="+72"/>
+        <source>Media type: %1
+Media size: %2
+</source>
+        <translation>Тип файла: %1
+Размер файла: %2
+</translation>
+    </message>
+</context>
+<context>
+    <name>dialogs::ReCaptcha</name>
+    <message>
+        <location filename="../../src/dialogs/ReCaptcha.cpp" line="+34"/>
+        <source>CONFIRM</source>
+        <translation>ПОДТВЕРДИТЬ</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>CANCEL</source>
+        <translation>ОТМЕНА</translation>
+    </message>
+    <message>
+        <location line="+11"/>
+        <source>Solve the reCAPTCHA and press the confirm button</source>
+        <translation>Решите reCAPTCHA и нажмите кнопку подтверждения</translation>
+    </message>
+</context>
+<context>
+    <name>dialogs::ReadReceipts</name>
+    <message>
+        <location filename="../../src/dialogs/ReadReceipts.cc" line="+98"/>
+        <source>Read receipts</source>
+        <translation>Подтверждать прочтение</translation>
+    </message>
+</context>
+<context>
+    <name>dialogs::RoomSettings</name>
+    <message>
+        <location filename="../../src/dialogs/RoomSettings.cpp" line="+54"/>
+        <source>CANCEL</source>
+        <translation>ОТМЕНА</translation>
+    </message>
+    <message>
+        <location line="+12"/>
+        <source>Notifications</source>
+        <translation>Уведомления</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Muted</source>
+        <translation>Заглушено</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Mentions only</source>
+        <translation>Только упоминания</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>All messages</source>
+        <translation>Все сообщения</translation>
+    </message>
+    <message>
+        <location line="+8"/>
+        <source>Room access</source>
+        <translation>Доступ к комнате</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Anyone and guests</source>
+        <translation>Все включая гостей</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Anyone</source>
+        <translation>Все</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Invited users</source>
+        <translation>Приглашённые пользователи</translation>
+    </message>
+</context>
+<context>
+    <name>emoji::Panel</name>
+    <message>
+        <location filename="../../src/emoji/Panel.cc" line="+125"/>
+        <source>Smileys &amp; People</source>
+        <translation>Рожицы и люди</translation>
+    </message>
+    <message>
+        <location line="+4"/>
+        <source>Animals &amp; Nature</source>
+        <translation>Животные и природа</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Food &amp; Drink</source>
+        <translation>Еда и напитки</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Activity</source>
+        <translation>Деятельность</translation>
+    </message>
+    <message>
+        <location line="+4"/>
+        <source>Travel &amp; Places</source>
+        <translation>Места и путешествия</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Objects</source>
+        <translation>Предметы</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Symbols</source>
+        <translation>Символы</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Flags</source>
+        <translation>Флаги</translation>
+    </message>
+</context>
+</TS>
