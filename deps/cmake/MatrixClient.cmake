b758cc4487837b93dacac9605a1668543a79a221
Konstantinos Sideris <sideris.konstantin@gmail.com>
2018-06-15 02:25:09 +0300

    Pass down toolchain file on Windows

--- deps/cmake/MatrixClient.cmake
+++ deps/cmake/MatrixClient.cmake
@@ -22,6 +22,7 @@ ExternalProject_Add(
         -DCMAKE_BUILD_TYPE=Release 
         -DBUILD_LIB_TESTS=OFF
         -DBUILD_LIB_EXAMPLES=OFF
+        -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
         ${PLATFORM_FLAGS}
         ${DEPS_BUILD_DIR}/mtxclient
   BUILD_COMMAND 
