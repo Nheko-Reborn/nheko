1a3a9c836cffa5d3d944f103970e5671f88544ac
Marcin Mikołajczak <me@m4sk.in>
2018-06-22 01:20:17 +0200

    i18n: Add Polish translation (#349)
    Signed-off-by: Marcin Mikołajczak <me@m4sk.in>

---
+++ resources/langs/nheko_pl.ts
@@ -0,0 +1,582 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pl">
+<context>
+    <name>AudioItem</name>
+    <message>
+        <location filename="../../src/timeline/widgets/AudioItem.cc" line="+125"/>
+        <source>Save File</source>
+        <translation>Zapisz plik</translation>
+    </message>
+</context>
+<context>
+    <name>DateSeparator</name>
+    <message>
+        <location filename="../../src/timeline/TimelineView.cc" line="+54"/>
+        <source>Today</source>
+        <translation>Dzisiaj</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Yesterday</source>
+        <translation>Wczoraj</translation>
+    </message>
+</context>
+<context>
+    <name>EditModal</name>
+    <message>
+        <location filename="../../src/dialogs/RoomSettings.cpp" line="+34"/>
+        <source>APPLY</source>
+        <translation>ZASTOSUJ</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>CANCEL</source>
+        <translation>ANULUJ</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>Name</source>
+        <translation>Nazwa</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Topic</source>
+        <translation>Temat</translation>
+    </message>
+</context>
+<context>
+    <name>FileItem</name>
+    <message>
+        <location filename="../../src/timeline/widgets/FileItem.cc" line="+111"/>
+        <source>Save File</source>
+        <translation>Zapisz plik</translation>
+    </message>
+</context>
+<context>
+    <name>ImageItem</name>
+    <message>
+        <location filename="../../src/timeline/widgets/ImageItem.cc" line="+229"/>
+        <source>Save image</source>
+        <translation>Zapisz obraz</translation>
+    </message>
+</context>
+<context>
+    <name>LoginPage</name>
+    <message>
+        <location filename="../../src/LoginPage.cc" line="+79"/>
+        <source>Matrix ID</source>
+        <translation>ID Matrixa</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>e.g @joe:matrix.org</source>
+        <translation>np. @joe:matrix.org</translation>
+    </message>
+    <message>
+        <location line="+15"/>
+        <source>Password</source>
+        <translation>Hasło</translation>
+    </message>
+    <message>
+        <location line="+19"/>
+        <source>LOGIN</source>
+        <translation>ZALOGUJ</translation>
+    </message>
+    <message>
+        <location line="+128"/>
+        <source>Empty password</source>
+        <translation>Puste hasło</translation>
+    </message>
+</context>
+<context>
+    <name>MatrixClient</name>
+    <message>
+        <location filename="../../src/MatrixClient.cc" line="+164"/>
+        <source>Wrong username or password</source>
+        <translation>Nieprawidłowa nazwa użytkownika lub hasło</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>Login endpoint was not found on the server</source>
+        <translation>Nie znaleziono punktu końcowego logowania na serwerze</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>An unknown error occured. Please try again.</source>
+        <translation>Wystąpił nieznany błąd. Spróbuj ponownie.</translation>
+    </message>
+    <message>
+        <location line="+23"/>
+        <source>Malformed response. Possibly not a Matrix server</source>
+        <translation>Nieprawidłowa odpowiedź. Może to nie być serwer Matrixa</translation>
+    </message>
+</context>
+<context>
+    <name>MemberList</name>
+    <message>
+        <location filename="../../src/dialogs/MemberList.cpp" line="+79"/>
+        <source>Room members</source>
+        <translation>Członkowie pokoju</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>SHOW MORE</source>
+        <translation>POKAŻ WIĘCEJ</translation>
+    </message>
+</context>
+<context>
+    <name>QuickSwitcher</name>
+    <message>
+        <location filename="../../src/QuickSwitcher.cc" line="+70"/>
+        <source>Search for a room...</source>
+        <translation>Wyszukaj pokoju…</translation>
+    </message>
+</context>
+<context>
+    <name>RegisterPage</name>
+    <message>
+        <location filename="../../src/RegisterPage.cc" line="+76"/>
+        <source>Username</source>
+        <translation>Nazwa użytkownika</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Password</source>
+        <translation>Hasło</translation>
+    </message>
+    <message>
+        <location line="+4"/>
+        <source>Password confirmation</source>
+        <translation>Potwierdzenie hasła</translation>
+    </message>
+    <message>
+        <location line="+4"/>
+        <source>Home Server</source>
+        <translation>Serwer domowy</translation>
+    </message>
+    <message>
+        <location line="+17"/>
+        <source>REGISTER</source>
+        <translation>ZAREJESTRUJ</translation>
+    </message>
+    <message>
+        <location line="+76"/>
+        <source>Invalid username</source>
+        <translation>Nieprawidłowa nazwa użytkownika</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Password is not long enough (min 8 chars)</source>
+        <translation>Hasło jest zbyt krótkie (min. 8 znaków)</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Passwords don&apos;t match</source>
+        <translation>Hasła nie pasują do siebie</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Invalid server name</source>
+        <translation>Nieprawidłowa nazwa serwera</translation>
+    </message>
+</context>
+<context>
+    <name>RoomInfoListItem</name>
+    <message>
+        <location filename="../../src/RoomInfoListItem.cc" line="+78"/>
+        <source>Leave room</source>
+        <translation>Opuść pokój</translation>
+    </message>
+    <message>
+        <location line="+153"/>
+        <source>Accept</source>
+        <translation>Akceptuj</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Decline</source>
+        <translation>Odrzuć</translation>
+    </message>
+</context>
+<context>
+    <name>SideBarActions</name>
+    <message>
+        <location filename="../../src/SideBarActions.cc" line="+36"/>
+        <source>Create new room</source>
+        <translation>Utwórz nowy pokój</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Join a room</source>
+        <translation>Dołącz do pokoju</translation>
+    </message>
+</context>
+<context>
+    <name>TextInputWidget</name>
+    <message>
+        <location filename="../../src/TextInputWidget.cc" line="+445"/>
+        <source>Write a message...</source>
+        <translation>Napisz wiadomość…</translation>
+    </message>
+    <message>
+        <location line="+108"/>
+        <source>Select a file</source>
+        <translation>Wybierz plik</translation>
+    </message>
+    <message>
+        <location line="+0"/>
+        <source>All Files (*)</source>
+        <translation>Wszystkie pliki (*)</translation>
+    </message>
+</context>
+<context>
+    <name>TopRoomBar</name>
+    <message>
+        <location filename="../../src/TopRoomBar.cc" line="+87"/>
+        <source>Invite users</source>
+        <translation>Zaproś użytkowników</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Members</source>
+        <translation>Członkowie</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>Leave room</source>
+        <translation>Opuść pokój</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>Settings</source>
+        <translation>Ustawienia</translation>
+    </message>
+</context>
+<context>
+    <name>TrayIcon</name>
+    <message>
+        <location filename="../../src/TrayIcon.cc" line="+116"/>
+        <source>Show</source>
+        <translation>Pokaż</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Quit</source>
+        <translation>Zakończ</translation>
+    </message>
+</context>
+<context>
+    <name>TypingDisplay</name>
+    <message>
+        <location filename="../../src/TypingDisplay.cc" line="+26"/>
+        <source> is typing</source>
+        <translation> pisze</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source> are typing</source>
+        <translation> piszą</translation>
+    </message>
+</context>
+<context>
+    <name>UserSettingsPage</name>
+    <message>
+        <location filename="../../src/UserSettingsPage.cc" line="+121"/>
+        <source>User Settings</source>
+        <translation>Ustawienia użytkownika</translation>
+    </message>
+    <message>
+        <location line="+15"/>
+        <source>Minimize to tray</source>
+        <translation>Zminimalizuj do paska zadań</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>Start in tray</source>
+        <translation>Rozpocznij na pasku zadań</translation>
+    </message>
+    <message>
+        <location line="+12"/>
+        <source>Re-order rooms based on activity</source>
+        <translation>Porządkuj pokoje na podstawie aktywności</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>Group&apos;s sidebar</source>
+        <translation>Pasek boczny grupy</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>Typing notifications</source>
+        <translation>Powiadomienia o pisaniu</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>Read receipts</source>
+        <translation>Potwierdzenia przeczytania</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>Theme</source>
+        <translation>Motyw</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>GENERAL</source>
+        <translation>OGÓLNE</translation>
+    </message>
+</context>
+<context>
+    <name>WelcomePage</name>
+    <message>
+        <location filename="../../src/WelcomePage.cc" line="+44"/>
+        <source>Welcome to nheko! The desktop client for the Matrix protocol.</source>
+        <translation>Witamy w nheko! Desktopowy klient protokołu Matrix.</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Enjoy your stay!</source>
+        <translation>Udanego pobytu!</translation>
+    </message>
+    <message>
+        <location line="+19"/>
+        <source>REGISTER</source>
+        <translation>ZAREJESTRUJ SIĘ</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>LOGIN</source>
+        <translation>ZALOGUJ SIĘ</translation>
+    </message>
+</context>
+<context>
+    <name>dialogs::CreateRoom</name>
+    <message>
+        <location filename="../../src/dialogs/CreateRoom.cc" line="+32"/>
+        <source>CANCEL</source>
+        <translation>ANULUJ</translation>
+    </message>
+    <message>
+        <location line="+11"/>
+        <source>Name</source>
+        <translation>Nazwa</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Topic</source>
+        <translation>Temat</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Alias</source>
+        <translation>Alias</translation>
+    </message>
+    <message>
+        <location line="+8"/>
+        <source>Room Visibility</source>
+        <translation>Widoczność pokoju</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>Room Preset</source>
+        <translation>Preset pokoju</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>Direct Chat</source>
+        <translation>Czat bezpośredni</translation>
+    </message>
+</context>
+<context>
+    <name>dialogs::InviteUsers</name>
+    <message>
+        <location filename="../../src/dialogs/InviteUsers.cc" line="+36"/>
+        <source>CANCEL</source>
+        <translation>ANULUJ</translation>
+    </message>
+    <message>
+        <location line="+11"/>
+        <source>User ID to invite</source>
+        <translation>ID użytkownika do zaproszenia</translation>
+    </message>
+</context>
+<context>
+    <name>dialogs::JoinRoom</name>
+    <message>
+        <location filename="../../src/dialogs/JoinRoom.cc" line="+30"/>
+        <source>CANCEL</source>
+        <translation>ANULUJ</translation>
+    </message>
+    <message>
+        <location line="+11"/>
+        <source>Room ID or alias</source>
+        <translation>ID pokoju lub alias</translation>
+    </message>
+</context>
+<context>
+    <name>dialogs::LeaveRoom</name>
+    <message>
+        <location filename="../../src/dialogs/LeaveRoom.cc" line="+29"/>
+        <source>CANCEL</source>
+        <translation>ANULUJ</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>Are you sure you want to leave?</source>
+        <translation>Czy na pewno chcesz wyjść?</translation>
+    </message>
+</context>
+<context>
+    <name>dialogs::Logout</name>
+    <message>
+        <location filename="../../src/dialogs/Logout.cc" line="+47"/>
+        <source>CANCEL</source>
+        <translation>ANULUJ</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>Logout. Are you sure?</source>
+        <translation>Czy na pewno chcesz wylogować się?</translation>
+    </message>
+</context>
+<context>
+    <name>dialogs::PreviewUploadOverlay</name>
+    <message>
+        <location filename="../../src/dialogs/PreviewUploadOverlay.cc" line="+41"/>
+        <source>Upload</source>
+        <translation>Wyślij</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Cancel</source>
+        <translation>Anuluj</translation>
+    </message>
+    <message>
+        <location line="+72"/>
+        <source>Media type: %1
+Media size: %2
+</source>
+        <translation>Rodzaj multimediów: %1
+Rozmiar multimediów: %2
+</translation>
+    </message>
+</context>
+<context>
+    <name>dialogs::ReCaptcha</name>
+    <message>
+        <location filename="../../src/dialogs/ReCaptcha.cpp" line="+34"/>
+        <source>CONFIRM</source>
+        <translation>POTWIERDŹ</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>CANCEL</source>
+        <translation>ANULUJ</translation>
+    </message>
+    <message>
+        <location line="+11"/>
+        <source>Solve the reCAPTCHA and press the confirm button</source>
+        <translation>Rozwiąż reCAPTCHA i naciśnij przycisk „potwierdź”</translation>
+    </message>
+</context>
+<context>
+    <name>dialogs::ReadReceipts</name>
+    <message>
+        <location filename="../../src/dialogs/ReadReceipts.cc" line="+98"/>
+        <source>Read receipts</source>
+        <translation>Potwierdzenia przeczytania</translation>
+    </message>
+</context>
+<context>
+    <name>dialogs::RoomSettings</name>
+    <message>
+        <location filename="../../src/dialogs/RoomSettings.cpp" line="+147"/>
+        <source>CANCEL</source>
+        <translation>ANULUJ</translation>
+    </message>
+    <message>
+        <location line="+12"/>
+        <source>Notifications</source>
+        <translation>Powiadomienia</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Muted</source>
+        <translation>Wyciszone</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Mentions only</source>
+        <translation>Tylko wspomnienia</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>All messages</source>
+        <translation>Wszystkie wiadomości</translation>
+    </message>
+    <message>
+        <location line="+8"/>
+        <source>Room access</source>
+        <translation>Dostęp do pokoju</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Anyone and guests</source>
+        <translation>Wszyscy i goście</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Anyone</source>
+        <translation>Wszyscy</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Invited users</source>
+        <translation>Zaproszeni użytkownicy</translation>
+    </message>
+</context>
+<context>
+    <name>emoji::Panel</name>
+    <message>
+        <location filename="../../src/emoji/Panel.cc" line="+125"/>
+        <source>Smileys &amp; People</source>
+        <translation>Twarze i ludzie</translation>
+    </message>
+    <message>
+        <location line="+4"/>
+        <source>Animals &amp; Nature</source>
+        <translation>Zwierzęta i natura</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Food &amp; Drink</source>
+        <translation>Jedzenie i napoje</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Activity</source>
+        <translation>Aktywność</translation>
+    </message>
+    <message>
+        <location line="+4"/>
+        <source>Travel &amp; Places</source>
+        <translation>Podróże i miejsca</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Objects</source>
+        <translation>Objekty</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Symbols</source>
+        <translation>Symbole</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Flags</source>
+        <translation>Flagi</translation>
+    </message>
+</context>
+</TS>
