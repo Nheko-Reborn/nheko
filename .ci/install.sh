2749afbb91116c5f45989d4ab0073acbebbf8c7d
Konstantinos Sideris <sideris.konstantin@gmail.com>
2018-06-22 23:54:35 +0300

    Fix the appimage generation

--- .ci/install.sh
+++ .ci/install.sh
@@ -5,7 +5,7 @@ set -ex
 if [ $TRAVIS_OS_NAME == osx ]; then
     brew update
     brew install qt5 lmdb clang-format ninja libsodium spdlog
-    brew upgrade boost
+    brew upgrade boost cmake
 
     curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
     sudo python get-pip.py
@@ -24,16 +24,17 @@ if [ $TRAVIS_OS_NAME == linux ]; then
         QT_PKG="59"
     fi
 
+    wget https://cmake.org/files/v3.11/cmake-3.11.4-Linux-x86_64.sh
+    sudo sh cmake-3.11.4-Linux-x86_64.sh  --skip-license  --prefix=/usr/local
+
     sudo add-apt-repository -y ppa:chris-lea/libsodium
     sudo add-apt-repository -y ppa:beineri/opt-qt${QT_VERSION}-trusty
-    sudo add-apt-repository -y ppa:george-edison55/cmake-3.x
     sudo apt-get update -qq
     sudo apt-get install -qq -y \
         qt${QT_PKG}base \
         qt${QT_PKG}tools \
         qt${QT_PKG}svg \
         qt${QT_PKG}multimedia \
-        cmake \
         liblmdb-dev \
         libsodium-dev
 fi
--- .ci/linux/deploy.sh
+++ .ci/linux/deploy.sh
@@ -32,6 +32,10 @@ unset LD_LIBRARY_PATH
 
 export ARCH=$(uname -m)
 
+if [ ! -z $TRAVIS_TAG ]; then
+    export LD_LIBRARY_PATH=.deps/usr/lib/:/opt/qt${QT_PKG}/lib/:$LD_LIBRARY_PATH
+fi
+
 ./linuxdeployqt*.AppImage ${DIR}/usr/share/applications/*.desktop -bundle-non-qt-libs
 ./linuxdeployqt*.AppImage ${DIR}/usr/share/applications/*.desktop -appimage
 
