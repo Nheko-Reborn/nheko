2db77173858016051843c2270f6cce14f4e25193
Konstantinos Sideris <sideris.konstantin@gmail.com>
2018-09-18 17:03:05 +0300

    Install libsodium 1.0.16

--- .ci/install.sh
+++ .ci/install.sh
@@ -27,7 +27,14 @@ if [ $TRAVIS_OS_NAME == linux ]; then
     wget https://cmake.org/files/v3.12/cmake-3.12.2-Linux-x86_64.sh
     sudo sh cmake-3.12.2-Linux-x86_64.sh  --skip-license  --prefix=/usr/local
 
-    sudo add-apt-repository -y ppa:chris-lea/libsodium
+    mkdir -p build-libsodium
+    pushd build-libsodium
+    curl -L https://download.libsodium.org/libsodium/releases/libsodium-1.0.16.tar.gz -o libsodium-1.0.16.tar.gz
+    tar xfz libsodium-1.0.16.tar.gz 
+    cd libsodium-1.0.16/
+    ./configure && make && make check && sudo make install
+    popd
+
     sudo add-apt-repository -y ppa:beineri/opt-qt${QT_VERSION}-trusty
     sudo apt-get update -qq
     sudo apt-get install -qq -y \
@@ -35,6 +42,5 @@ if [ $TRAVIS_OS_NAME == linux ]; then
         qt${QT_PKG}tools \
         qt${QT_PKG}svg \
         qt${QT_PKG}multimedia \
-        liblmdb-dev \
-        libsodium-dev
+        liblmdb-dev
 fi
