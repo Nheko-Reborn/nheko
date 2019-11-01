bdf6e2b0280d0825aa36394d1fb08f3cc30f8a0c
Joseph Donofry <red.sky@nheko.im>
2019-11-01 21:30:56 +0000

    Fix boost build in FreeBSD

--- deps/cmake/Boost.cmake
+++ deps/cmake/Boost.cmake
@@ -3,6 +3,10 @@ if(WIN32)
   return()
 endif()
 
+include(BoostToolsetId)
+set(BOOST_TOOLSET "gcc")
+Boost_Get_ToolsetId(BOOST_TOOLSET)
+
 ExternalProject_Add(
   Boost
 
@@ -16,6 +20,7 @@ ExternalProject_Add(
   CONFIGURE_COMMAND ${DEPS_BUILD_DIR}/boost/bootstrap.sh
     --with-libraries=random,thread,system,iostreams,atomic,chrono,date_time,regex
     --prefix=${DEPS_INSTALL_DIR}
+    --with-toolset=${BOOST_TOOLSET}
   BUILD_COMMAND ${DEPS_BUILD_DIR}/boost/b2 -d0 cxxstd=14 variant=release link=shared runtime-link=shared threading=multi --layout=system
   INSTALL_COMMAND ${DEPS_BUILD_DIR}/boost/b2 -d0 install
 )
