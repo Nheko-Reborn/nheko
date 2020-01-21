6aedd135a96417e014d29fe7114f70c9d60c34df
Nicolas Werner <nicolas.werner@hotmail.de>
2020-01-21 22:12:59 +0100

    Remove useless logging

--- src/ColorImageProvider.cpp
+++ src/ColorImageProvider.cpp
@@ -1,14 +1,11 @@
 #include "ColorImageProvider.h"
 
-#include "Logging.h"
 #include <QPainter>
 
 QPixmap
 ColorImageProvider::requestPixmap(const QString &id, QSize *size, const QSize &)
 {
         auto args = id.split('?');
-
-        nhlog::ui()->info("Loading {}, source is {}", id.toStdString(), args[0].toStdString());
 
         QPixmap source(args[0]);
 
