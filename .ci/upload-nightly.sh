ff2a49f0cb6594851b9d973d0b14b9711e081b41
Nicolas Werner <nicolas.werner@hotmail.de>
2020-02-29 02:56:33 +0100

    Test matrix nightly uploader

---
+++ .ci/upload-nightly.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+file=$(find artifacts/ -type f -exec basename {} \;)
+
+uri=$(curl -H "Authorization: Bearer ${MATRIX_ACCESS_TOKEN}" -H "Content-Type: application/x-compressed" -X POST --data-binary "@artifacts/${file}" "https://matrix.neko.dev/_matrix/media/r0/upload?filename=${file}" | python3 -c "import sys, json; print(json.load(sys.stdin)['content_uri'])")
+echo "Uploaded to ${uri}"
+
+curl -H "Authorization: Bearer ${MATRIX_ACCESS_TOKEN}" -H "Content-Type: application/json" -X PUT -d "{ \"body\": \"${file}\", \"filename\": \"${file}\", \"info\": { \"mimetype\": \"application/x-compressed\", \"size\": $(wc -c < artifacts/${file}) }, \"msgtype\": \"m.file\", \"url\": \"${uri}\" }" "https://matrix.neko.dev/_matrix/client/r0/rooms/${ROOM}/send/m.room.message/${RANDOM}"
