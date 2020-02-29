cc3a42e502f6a1d4a5db57b8572f94219b3b89ce
Nicolas Werner <nicolas.werner@hotmail.de>
2020-02-29 15:25:52 +0100

    appveyor upload use headers instead of ps6 authourization flags

--- .ci/upload-nightly.ps1
+++ .ci/upload-nightly.ps1
@@ -1,7 +1,7 @@
 $file = "nheko_win_64.zip"
 $fileName = "nheko-${env:APPVEYOR_REPO_COMMIT}-win64.zip"
 
-$response = Invoke-RestMethod -uri "https://matrix.neko.dev/_matrix/media/r0/upload?filename=$fileName" -Method Post -Infile "$file" -ContentType 'application/x-compressed' -Authentication Bearer -Token ${env:MATRIX_ACCESS_TOKEN}
+$response = Invoke-RestMethod -uri "https://matrix.neko.dev/_matrix/media/r0/upload?filename=$fileName" -Method Post -Infile "$file" -ContentType 'application/x-compressed' -Headers @{"Authorization"="Bearer ${env:MATRIX_ACCESS_TOKEN}"}
 
 $txId = [DateTimeOffset]::Now.ToUnixTimeSeconds()
 $fileSize = (Get-Item $file).Length
@@ -17,6 +17,5 @@ $body = @{
 } | ConvertTo-Json
 $room = "!TshDrgpBNBDmfDeEGN:neko.dev"
 
-Invoke-RestMethod -uri "https://matrix.neko.dev/_matrix/client/r0/rooms/${room}/send/m.room.message/${txid}" -Method Put -Body "$body" -ContentType 'application/json' -Authentication Bearer -Token "${env:MATRIX_ACCESS_TOKEN}"
-
+Invoke-RestMethod -uri "https://matrix.neko.dev/_matrix/client/r0/rooms/${room}/send/m.room.message/${txid}" -Method Put -Body "$body" -ContentType 'application/json' -Headers @{"Authorization"="Bearer ${env:MATRIX_ACCESS_TOKEN}"}
 
