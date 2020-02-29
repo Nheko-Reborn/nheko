5750da735258c8db67f840a5656713875c459b32
Nicolas Werner <nicolas.werner@hotmail.de>
2020-02-29 16:38:40 +0100

    Use timestamp for txid on upload instead of RANDOM

--- .ci/upload-nightly.sh
+++ .ci/upload-nightly.sh
@@ -5,4 +5,4 @@ file=$(find artifacts/ -type f -exec basename {} \;)
 uri=$(curl -H "Authorization: Bearer ${MATRIX_ACCESS_TOKEN}" -H "Content-Type: application/x-compressed" -X POST --data-binary "@artifacts/${file}" "https://matrix.neko.dev/_matrix/media/r0/upload?filename=${file}"  --http1.1 | python -c "import sys, json; print(json.load(sys.stdin)['content_uri'])")
 echo "Uploaded to ${uri}"
 
-curl -H "Authorization: Bearer ${MATRIX_ACCESS_TOKEN}" -H "Content-Type: application/json" -X PUT -d "{ \"body\": \"${file}\", \"filename\": \"${file}\", \"info\": { \"mimetype\": \"application/x-compressed\", \"size\": $(wc -c < artifacts/${file}) }, \"msgtype\": \"m.file\", \"url\": \"${uri}\" }" "https://matrix.neko.dev/_matrix/client/r0/rooms/${ROOM}/send/m.room.message/${RANDOM}"
+curl -H "Authorization: Bearer ${MATRIX_ACCESS_TOKEN}" -H "Content-Type: application/json" -X PUT -d "{ \"body\": \"${file}\", \"filename\": \"${file}\", \"info\": { \"mimetype\": \"application/x-compressed\", \"size\": $(wc -c < artifacts/${file}) }, \"msgtype\": \"m.file\", \"url\": \"${uri}\" }" "https://matrix.neko.dev/_matrix/client/r0/rooms/${ROOM}/send/m.room.message/$(date +%s)"
