57b8cdbd4d94bd0adf5c92f775227e04dd5f2bf1
CH Chethan Reddy <40890937+Chethan2k1@users.noreply.github.com>
2020-05-15 17:00:52 +0530

    remove #if defined __has_include

--- src/Cache.h
+++ src/Cache.h
@@ -22,12 +22,10 @@
 #include <QImage>
 #include <QString>
 
-#if defined __has_include
 #if __has_include(<lmdbxx/lmdb++.h>)
 #include <lmdbxx/lmdb++.h>
 #else
 #include <lmdb++.h>
-#endif
 #endif
 
 #include <mtx/responses.hpp>
