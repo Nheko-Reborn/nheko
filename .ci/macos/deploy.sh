79e7eac7fceeaa39496f7b6bbbeb0e370434b2a2
Joseph Donofry <joedonofry@gmail.com>
2020-12-08 16:32:04 -0500

    More MacOS CI Updates

--- .ci/macos/deploy.sh
+++ .ci/macos/deploy.sh
@@ -16,14 +16,16 @@ PATH=/usr/local/opt/qt/bin/:${PATH}
   mkdir -p nheko.app/Contents/Frameworks
   find "${ICU_LIB}" -type l -name "*.dylib" -exec cp -a -n {} nheko.app/Contents/Frameworks/ \; || true
 
-  sudo macdeployqt nheko.app -dmg -always-overwrite -qmldir=../resources/qml/
+  macdeployqt nheko.app -dmg -always-overwrite -qmldir=../resources/qml/
 
   user=$(id -nu)
-  sudo chown "${user}" nheko.dmg
+  chown "${user}" nheko.dmg
   mv nheko.dmg ..
 )
 
 dmgbuild -s ./.ci/macos/settings.json "Nheko" nheko.dmg
+
+VERSION=${CI_COMMIT_SHORT_SHA}
 
 if [ -n "$VERSION" ]; then
     mv nheko.dmg "nheko-${VERSION}.dmg"
