c856840284b367b39f0e9931a493a54b233397f9
Nicolas Werner <nicolas.werner@hotmail.de>
2021-12-16 06:22:30 +0100

    Install qt5

--- .ci/windows/build-meson.cmd
+++ .ci/windows/build-meson.cmd
@@ -4,6 +4,9 @@ set PATH=%PATH:C:\cygwin64\bin;=%
 set PATH=C:\ProgramData\chocolatey\bin;C:\Python310;%QT_DIR%\bin;%PATH%
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64
 
+choco install -y 7zip
+pip uninstall -y aqtinstall && pip install --only-binary :all: aqtinstall && aqt install -E 7z --outputdir C:\Qt 5.15.2 windows desktop win64_msvc2019_64 -m qtcharts qtscript
+
 dir \
 dir \Qt\
 dir \Qt\5.15.2\
