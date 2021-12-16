02c1be496d644a746bf83ca4f171422288c24a8f
Nicolas Werner <nicolas.werner@hotmail.de>
2021-12-16 05:31:14 +0100

    Windows CI for meson

--- .ci/windows/build-meson.cmd
+++ .ci/windows/build-meson.cmd
@@ -4,14 +4,22 @@ set PATH=%PATH:C:\cygwin64\bin;=%
 set PATH=C:\ProgramData\chocolatey\bin;C:\Python310;%QT_DIR%\bin;%PATH%
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64
 
+dir \
+dir \Qt\
+dir \Qt\5.15.2\
+dir \Qt\5.15.2\msvc2019_64\
+dir \Qt\5.15.2\msvc2019_64\bin
+
 REM Build nheko
-meson setup builddir
-meson compile -C builddir
+pip install --upgrade meson || exit /b
+
+meson setup builddir || exit /b
+meson compile -C builddir || exit /b
 
 REM build qt-jdenticon
-git clone https://github.com/Nheko-Reborn/qt-jdenticon.git
+git clone https://github.com/Nheko-Reborn/qt-jdenticon.git || exit /b
 cd qt-jdenticon
-qmake
-nmake
+qmake || exit /b
+nmake || exit /b
 cd ..
 
