61404641d94bc6cedb39c62191ddea3af492c769
Nicolas Werner <nicolas.werner@hotmail.de>
2022-01-01 04:57:53 +0100

    Update year in copyright notice to shut up lint job

--- resources/qml/dialogs/RoomMembers.qml
+++ resources/qml/dialogs/RoomMembers.qml
@@ -1,4 +1,5 @@
 // SPDX-FileCopyrightText: 2021 Nheko Contributors
+// SPDX-FileCopyrightText: 2022 Nheko Contributors
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
--- src/dialogs/ImageOverlay.cpp
+++ src/dialogs/ImageOverlay.cpp
@@ -1,5 +1,6 @@
 // SPDX-FileCopyrightText: 2017 Konstantinos Sideris <siderisk@auth.gr>
 // SPDX-FileCopyrightText: 2021 Nheko Contributors
+// SPDX-FileCopyrightText: 2022 Nheko Contributors
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
