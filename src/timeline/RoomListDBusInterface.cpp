903623aba3ed692916c02780382facd1a2397d2a
Loren Burkholder <computersemiexpert@outlook.com>
2022-02-04 21:02:07 -0500

    Split out RoomListDBusInterface

---
+++ src/timeline/RoomListDBusInterface.cpp
@@ -0,0 +1,109 @@
+// SPDX-FileCopyrightText: 2022 Nheko Contributors
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include "RoomListDBusInterface.h"
+
+#include "Cache_p.h"
+#include "ChatPage.h"
+#include "Logging.h"
+#include "MainWindow.h"
+#include "MxcImageProvider.h"
+#include "RoomlistModel.h"
+
+#include <QDBusConnection>
+
+RoomListDBusInterface::RoomListDBusInterface(RoomlistModel *parent)
+  : QObject{parent}
+  , m_parent{parent}
+{
+    connect(ChatPage::instance(), &ChatPage::newRoom, this, &RoomListDBusInterface::prepareModel);
+    connect(ChatPage::instance(), &ChatPage::leftRoom, this, &RoomListDBusInterface::prepareModel);
+    prepareModel();
+}
+
+QVector<nheko::dbus::RoomInfoItem>
+RoomListDBusInterface::getRooms(const QDBusMessage &message)
+{
+    auto reply = message.createReply();
+
+    m_modelAccess.lock();
+    nhlog::ui()->debug("Sending {} rooms over D-Bus...", m_model->size());
+    reply << QVariant::fromValue(*m_model);
+    QDBusConnection::sessionBus().send(reply);
+    nhlog::ui()->debug("Rooms successfully sent to D-Bus.");
+    m_modelAccess.unlock();
+
+    return {};
+}
+
+void
+RoomListDBusInterface::activateRoom(const QString &alias) const
+{
+    bringWindowToTop();
+    m_parent->setCurrentRoom(alias);
+}
+
+void
+RoomListDBusInterface::joinRoom(const QString &alias) const
+{
+    bringWindowToTop();
+    ChatPage::instance()->joinRoom(alias);
+}
+
+void
+RoomListDBusInterface::prepareModel()
+{
+    m_modifyStagingDataMutex.lock();
+    static int modelSize;
+    modelSize = m_parent->models.size();
+    if (modelSize == 0) {
+        m_modifyStagingDataMutex.unlock();
+        return;
+    }
+
+    for (const auto &model : std::as_const(m_parent->models)) {
+        MainWindow::instance()->imageProvider()->download(
+          model->roomAvatarUrl().remove("mxc://"),
+          {128, 128},
+          [this,
+           &model](const QString &, const QSize &, const QImage &image, const QString &) mutable {
+              const auto aliases = cache::client()->getRoomAliases(model->roomId().toStdString());
+              QString alias;
+              if (aliases.has_value()) {
+                  const auto &val = aliases.value();
+                  if (!val.alias.empty())
+                      alias = QString::fromStdString(val.alias);
+                  else if (val.alt_aliases.size() > 0)
+                      alias = QString::fromStdString(val.alt_aliases.front());
+              }
+
+              m_addItemsToStagingData.lock();
+              m_stagingModel->push_back(
+                nheko::dbus::RoomInfoItem{model->roomId(), model->roomName(), alias, image});
+              m_addItemsToStagingData.unlock();
+
+              if (m_stagingModel->length() == modelSize) {
+                  m_modelAccess.lock();
+                  std::swap(m_model, m_stagingModel);
+                  m_modelAccess.unlock();
+
+                  m_stagingModel->clear();
+                  m_modifyStagingDataMutex.unlock();
+
+                  // If necessary, retrigger reloading of model. This is primarily aimed at startup
+                  // scenarios.
+                  if (m_model->size() != m_parent->models.size())
+                      prepareModel();
+              }
+          },
+          false);
+    }
+}
+
+void
+RoomListDBusInterface::bringWindowToTop() const
+{
+    MainWindow::instance()->show();
+    MainWindow::instance()->raise();
+}
--- src/timeline/RoomlistModel.h
+++ src/timeline/RoomlistModel.h
@@ -18,10 +18,7 @@
 #include "TimelineModel.h"
 
 #ifdef NHEKO_DBUS_SYS
-#include "NhekoDBusInterface.h"
-#include <QDBusMessage>
-#include <QDBusMetaType>
-#include <QMutex>
+#include "RoomListDBusInterface.h"
 #endif
 
 class TimelineViewManager;
@@ -47,51 +44,6 @@ public:
     QString roomid_, roomName_, roomAvatarUrl_, roomTopic_;
     bool isInvite_ = false;
 };
-
-#ifdef NHEKO_DBUS_SYS
-class RoomlistModel;
-
-class RoomListDBusInterface : public QObject
-{
-    Q_OBJECT
-
-public:
-    RoomListDBusInterface(RoomlistModel *parent);
-
-public slots:
-    //! Get the nheko D-Bus API version.
-    Q_SCRIPTABLE QString apiVersion() const { return nheko::dbus::apiVersion; }
-    //! Call this function to get a list of all joined rooms.
-    Q_SCRIPTABLE QVector<nheko::dbus::RoomInfoItem> getRooms(const QDBusMessage &message);
-    //! Activates a currently joined room.
-    Q_SCRIPTABLE void activateRoom(const QString &alias) const;
-    //! Joins a room. It is your responsibility to ask for confirmation (if desired).
-    Q_SCRIPTABLE void joinRoom(const QString &alias) const;
-
-private slots:
-    void prepareModel();
-
-private:
-    void bringWindowToTop() const;
-
-    RoomlistModel *m_parent;
-
-    // this is a QSharedPointer so that copy ops are less expensive (see below)
-    QSharedPointer<QVector<nheko::dbus::RoomInfoItem>> m_model{
-      new QVector<nheko::dbus::RoomInfoItem>};
-    // Use this to lock the entire model for access. This prevents potentially interesting race
-    // conditions when copying from the staging area.
-    QMutex m_modelAccess;
-
-    // use this to store data while generating a new model; then copy it to m_model
-    QSharedPointer<QVector<nheko::dbus::RoomInfoItem>> m_stagingModel{
-      new QVector<nheko::dbus::RoomInfoItem>};
-    // this locks the entire staging model during a refresh
-    QMutex m_modifyStagingDataMutex;
-    // use this to guard while adding individual items to the staging model
-    QMutex m_addItemsToStagingData;
-};
-#endif
 
 class RoomlistModel : public QAbstractListModel
 {
