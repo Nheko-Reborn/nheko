39dc3d0efbbadc3e2ead9a342a0685c4c482d1ad
Techcable <Techcable@techcable.net>
2022-08-03 13:50:00 -0700

    Port to Apple M1: Update needed dependencies
    See issue #563
    
    Requires update for hunter and spdlog,
    using a patched version of openssl (semi-endorsed by hunter since they don't have the full release yet)
    
    TODO:
    - Universal binaries
    - Get codesigning working (self-signing)
    
    The compilation command that (finally) got it to work:
    
    cmake -DHUNTER_STATUS_DEBUG=ON  -GNinja -H. -Bbuild -DCMAKE_OSX_ARCHITECTURES=arm64 -DCMAKE_SYSTEM_PROCESSOR=arm64 -DCMAKE_BUILD_T
    YPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=.deps/usr -DHUNTER_ROOT="../.hunter" -DHUNTER_ENABLED=ON -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=RelWithDebInfo -DHUN
    TER_CONFIGURATION_TYPES=RelWithDebInfo -DUSE_BUNDLED_OPENSSL=ON -DUSE_BUNDLED_BOOST=ON -DUSE_BUNDLED_MTXCLIENT=ON -DUSE_BUNDLED_JSON=ON -DUSE_BUNDLED_CMARK=ON -D
    USE_BUNDLED_COEURL=true

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,7 +1,7 @@
 
 hunter_config(
     spdlog
-    VERSION 1.8.0-p1
+    VERSION 1.9.2-p0
 )
 
 hunter_config(
@@ -9,10 +9,25 @@ hunter_config(
     VERSION 0.9.21-p2
 )
 
-hunter_config(
-    OpenSSL
-    VERSION 1.1.1j
-)
+# TODO: Update to 1.1.1q (latest release of OpenSSL) once hunter supports it
+# Then we can remove hacky version for Apple ARM64
+if (APPLE)
+    set(CMAKE_OSX_ARCHITECTURES arm64)
+    hunter_config(
+        OpenSSL
+        # Modified version to supports apple arm64
+        #
+        # See PR cpp-pm/hunter#248
+        # According to docs "only use this version if you need
+        # ARM64 support before official release"
+        VERSION 1.1.1g-p0
+    )
+else()
+    hunter_config(
+        OpenSSL
+        VERSION 1.1.1j
+    )
+endif()
 
 hunter_config(
     Libevent
