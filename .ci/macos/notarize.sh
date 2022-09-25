e6bbe74abfb73003495da68e847e6af8397c5ae9
Joseph Donofry <rubberduckie3554@gmail.com>
2022-09-25 18:08:13 -0400

    Make sure there are no spaces in the status string

--- .ci/macos/notarize.sh
+++ .ci/macos/notarize.sh
@@ -56,23 +56,23 @@ xcrun notarytool submit nheko.dmg --apple-id "${APPLE_DEV_USER}" --password "${A
 requestUUID="$(awk -F ': ' '/id/ {print $2}' "$NOTARIZE_SUBMIT_LOG" | head -1)"
 
 if [ -z "${requestUUID}" ]; then
-  echo "Received requestUUID: ${requestUUID}"
+  echo "Received requestUUID: \"${requestUUID}\""
 else
   echo "Something went wrong when submitting the request... we don't have a UUID"
   exit 1
 fi
 
 while sleep 60 && date; do
-  echo "--> Checking notarization status for ${requestUUID}"
+  echo "--> Checking notarization status for \"${requestUUID}\""
 
   # OLD altool usage: xcrun altool --notarization-info "${requestUUID}" -u "${APPLE_DEV_USER}" -p "${APPLE_DEV_PASS}" > "$NOTARIZE_STATUS_LOG" 2>&1
   xcrun notarytool info "${requestUUID}" --apple-id "${APPLE_DEV_USER}" --password "${APPLE_DEV_PASS}" --team-id "${APPLE_TEAM_ID}" > "$NOTARIZE_STATUS_LOG" 2>&1
 
-  sub_status="$(awk -F ':' '/status/ {print $2}' "$NOTARIZE_STATUS_LOG")"
+  sub_status="$(awk -F ': ' '/status/ {print $2}' "$NOTARIZE_STATUS_LOG")"
   #isSuccess=$(grep "success" "$NOTARIZE_STATUS_LOG")
   #isFailure=$(grep "invalid" "$NOTARIZE_STATUS_LOG")
 
-  echo "Status for submission ${requestUUID}: ${sub_status}" 
+  echo "Status for submission \"${requestUUID}\": \"${sub_status}\"" 
 
   if [ "${sub_status}" = "Accepted" ]; then
       echo "Notarization done!"
