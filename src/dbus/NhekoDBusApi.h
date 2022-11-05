b622604c47aceeda62b20caf6553bbf1008b33f5
Loren Burkholder <computersemiexpert@outlook.com>
2022-11-05 16:38:12 -0400

    Bump micro version

--- src/dbus/NhekoDBusApi.h
+++ src/dbus/NhekoDBusApi.h
@@ -18,7 +18,7 @@ init();
 
 //! The nheko D-Bus API version provided by this file. The API version number follows semantic
 //! versioning as defined by https://semver.org.
-const QVersionNumber dbusApiVersion{1, 0, 0};
+const QVersionNumber dbusApiVersion{1, 0, 1};
 
 //! Compare the installed Nheko API to the version that your client app targets to see if they
 //! are compatible.
