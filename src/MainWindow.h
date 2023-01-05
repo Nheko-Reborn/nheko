856226e9e81d4ca78e45a9c029beac30f1dd8570
Malte E <97891689+maltee1@users.noreply.github.com>
2023-01-05 08:11:44 +0800

    undo linter disagreement

--- src/MainWindow.h
+++ src/MainWindow.h
@@ -58,10 +58,7 @@ public:
     void showChatPage();
 
 #ifdef NHEKO_DBUS_SYS
-    bool dbusAvailable() const
-    {
-        return dbusAvailable_;
-    }
+    bool dbusAvailable() const { return dbusAvailable_; }
 #endif
 
     Q_INVOKABLE void addPerRoomWindow(const QString &room, QWindow *window);
