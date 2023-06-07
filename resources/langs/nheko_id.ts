656a5c1e88ef954b236d56cc57f42be0f0b40221
Weblate <noreply@weblate.org>
2023-06-07 19:08:31 +0000

    Update translation files
    Updated by "Cleanup translation files" hook in Weblate.
    
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://weblate.nheko.im/projects/nheko/nheko-master/
    Translation: Nheko/nheko

--- resources/langs/nheko_id.ts
+++ resources/langs/nheko_id.ts
@@ -1392,11 +1392,6 @@ Kamu dapat memberikan alasan untuk orang lain untuk menerima ketukanmu:</transla
 <context>
     <name>InputBar</name>
     <message>
-        <location filename="../../src/timeline/InputBar.cpp" line="+372"/>
-        <source>Select a file</source>
-        <translation>Pilih sebuah file</translation>
-    </message>
-    <message>
         <location line="+0"/>
         <source>All Files (*)</source>
         <translation>Semua File (*)</translation>
