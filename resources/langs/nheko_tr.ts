a7f1e31979ce13fa172e3d78905e7b023afdf5f8
Weblate <noreply@weblate.org>
2023-06-07 19:08:32 +0000

    Update translation files
    Updated by "Cleanup translation files" hook in Weblate.
    
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://weblate.nheko.im/projects/nheko/nheko-master/
    Translation: Nheko/nheko

--- resources/langs/nheko_tr.ts
+++ resources/langs/nheko_tr.ts
@@ -1394,11 +1394,6 @@ You may optionally provide a reason for others to accept your knock:</source>
 <context>
     <name>InputBar</name>
     <message>
-        <location filename="../../src/timeline/InputBar.cpp" line="+372"/>
-        <source>Select a file</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <location line="+0"/>
         <source>All Files (*)</source>
         <translation type="unfinished"></translation>
