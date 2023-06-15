024486c989eca3a7ae679ddd6bd3dd00a7aad94d
Joseph Donofry <rubberduckie3554@gmail.com>
2023-06-15 19:08:22 -0400

    Maybe fix macOS deployment (intel for now)

--- .ci/macos/build.sh
+++ .ci/macos/build.sh
@@ -14,7 +14,7 @@ export CMAKE_PREFIX_PATH
 
 cmake -GNinja -S. -Bbuild \
       -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-      -DCMAKE_INSTALL_PREFIX=.deps/usr \
+      -DCMAKE_INSTALL_PREFIX="$(pwd)" \
       -DHUNTER_ROOT="../.hunter" \
       -DHUNTER_ENABLED=ON -DBUILD_SHARED_LIBS=OFF \
       -DCMAKE_BUILD_TYPE=RelWithDebInfo -DHUNTER_CONFIGURATION_TYPES=RelWithDebInfo \
@@ -28,7 +28,8 @@ cmake --build build
     make -j 4
     cp libqtjdenticon.dylib ../nheko.app/Contents/MacOS
   )
-  "$(brew --prefix qt6)/bin/macdeployqt" nheko.app -always-overwrite -qmldir=../resources/qml/
-  # workaround for https://bugreports.qt.io/browse/QTBUG-100686
-  cp "$(brew --prefix brotli)/lib/libbrotlicommon.1.dylib" nheko.app/Contents/Frameworks/libbrotlicommon.1.dylib
+  # "$(brew --prefix qt6)/bin/macdeployqt" nheko.app -always-overwrite -qmldir=../resources/qml/
+  # # workaround for https://bugreports.qt.io/browse/QTBUG-100686
+  # cp "$(brew --prefix brotli)/lib/libbrotlicommon.1.dylib" nheko.app/Contents/Frameworks/libbrotlicommon.1.dylib
+  cmake --install build
 )
