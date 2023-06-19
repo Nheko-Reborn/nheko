b15d889da0fc0d8ac781034971de586589916ce5
Nicolas Werner <nicolas.werner@hotmail.de>
2023-06-19 19:30:47 +0200

    Fix double nested nheko folder

--- .ci/macos/build.sh
+++ .ci/macos/build.sh
@@ -14,7 +14,7 @@ export CMAKE_PREFIX_PATH
 
 cmake -GNinja -S. -Bbuild \
       -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-      -DCMAKE_INSTALL_PREFIX="nheko.app" \
+      -DCMAKE_INSTALL_PREFIX="nheko.temp" \
       -DHUNTER_ROOT="../.hunter" \
       -DHUNTER_ENABLED=ON -DBUILD_SHARED_LIBS=OFF \
       -DCMAKE_BUILD_TYPE=RelWithDebInfo -DHUNTER_CONFIGURATION_TYPES=RelWithDebInfo \
@@ -27,9 +27,11 @@ cmake --install build
   ( cd qt-jdenticon
     qmake
     make -j 4
-    cp libqtjdenticon.dylib ../../nheko.app/Contents/MacOS
+    cp libqtjdenticon.dylib ../../nheko.temp/nheko.app/Contents/MacOS
   )
   # "$(brew --prefix qt6)/bin/macdeployqt" nheko.app -always-overwrite -qmldir=../resources/qml/
   # # workaround for https://bugreports.qt.io/browse/QTBUG-100686
   # cp "$(brew --prefix brotli)/lib/libbrotlicommon.1.dylib" nheko.app/Contents/Frameworks/libbrotlicommon.1.dylib
 )
+
+mv nheko.temp/nheko.app nheko.app
