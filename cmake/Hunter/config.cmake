4faeae5c73d0151cff57facb28172318acda8231
Nicolas Werner <nicolas.werner@hotmail.de>
2023-10-29 19:57:22 +0100

    Update dependencies

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,28 +1,13 @@
 hunter_config(
-    lmdb
-    VERSION 0.9.21-p2
-)
-
-hunter_config(
-    OpenSSL
-    VERSION 1.1.1j
-)
-
-hunter_config(
-    Libevent
-    VERSION 2.1.8-p4
-)
-
-hunter_config(
     nlohmann_json
-    VERSION 3.8.0
+    VERSION 3.11.1
     CMAKE_ARGS JSON_MultipleHeaders=ON
 )
 
 if (WIN32)
     hunter_config(
         CURL
-        VERSION 7.74.0-p2
+        VERSION 8.4.0
         CMAKE_ARGS
             CMAKE_USE_SCHANNEL=ON
             BUILD_CURL_TESTS=OFF
@@ -34,7 +19,7 @@ if (WIN32)
 else()
     hunter_config(
         CURL
-        VERSION 7.74.0-p2
+        VERSION 8.4.0
         CMAKE_ARGS
             CMAKE_USE_SCHANNEL=OFF
             BUILD_CURL_TESTS=OFF
