8214e8dcf2761e57e98341435571c0bc57a47e5d
Nicolas Werner <nicolas.werner@hotmail.de>
2024-01-08 01:30:12 +0100

    Make clazy happy

--- src/ui/MxcMediaProxy.cpp
+++ src/ui/MxcMediaProxy.cpp
@@ -29,22 +29,24 @@ MxcMediaProxy::MxcMediaProxy(QObject *parent)
                              static_cast<int>(error),
                              errorString.toStdString());
       });
-    connect(this, &MxcMediaProxy::mediaStatusChanged, [this](QMediaPlayer::MediaStatus status) {
-        nhlog::ui()->info("Media player status {} and error {}",
-                          static_cast<int>(status),
-                          static_cast<int>(this->error()));
-    });
-    connect(this, &MxcMediaProxy::playbackStateChanged, [this](QMediaPlayer::PlaybackState status) {
-        // We only set the output when starting the playback because otherwise the audio device
-        // lookup takes about 500ms, which causes a lot of stutter...
-        if (status == QMediaPlayer::PlayingState && !audioOutput()) {
-            nhlog::ui()->debug("Set audio output");
-            auto newOut = new QAudioOutput(this);
-            newOut->setMuted(muted_);
-            newOut->setVolume(volume_);
-            setAudioOutput(newOut);
-        }
-    });
+    connect(
+      this, &MxcMediaProxy::mediaStatusChanged, this, [this](QMediaPlayer::MediaStatus status) {
+          nhlog::ui()->info("Media player status {} and error {}",
+                            static_cast<int>(status),
+                            static_cast<int>(this->error()));
+      });
+    connect(
+      this, &MxcMediaProxy::playbackStateChanged, this, [this](QMediaPlayer::PlaybackState status) {
+          // We only set the output when starting the playback because otherwise the audio device
+          // lookup takes about 500ms, which causes a lot of stutter...
+          if (status == QMediaPlayer::PlayingState && !audioOutput()) {
+              nhlog::ui()->debug("Set audio output");
+              auto newOut = new QAudioOutput(this);
+              newOut->setMuted(muted_);
+              newOut->setVolume(volume_);
+              setAudioOutput(newOut);
+          }
+      });
     connect(this, &MxcMediaProxy::metaDataChanged, [this]() { emit orientationChanged(); });
 
     connect(ChatPage::instance()->timelineManager()->rooms(),
