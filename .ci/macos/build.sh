0c33b64f41a3df961e59612ccd483e7f3b20f2e6
Joe Donofry <rubberduckie3554@gmail.com>
2025-06-16 22:45:33 -0400

    Fix typo

--- .ci/macos/build.sh
+++ .ci/macos/build.sh
@@ -26,7 +26,7 @@ cmake -GNinja -S. -Bbuild \
       -DHUNTER_ENABLED=ON -DBUILD_SHARED_LIBS=OFF \
       -DCMAKE_BUILD_TYPE=RelWithDebInfo -DHUNTER_CONFIGURATION_TYPES=RelWithDebInfo \
       -DQt6_DIR=${QT_BASEPATH}/lib/cmake \
-      -DCI_BUILD=ON \
+      -DCI_BUILD=ON
 cmake --build build
 cmake --install build
 ( cd build
